17bf27f7d84991abc8a9bea87dedb0677c38e221
Natanael Sirqueira <natanael.sirqueira@gmail.com>
2020-05-01 15:08:08 -0400

    Implementing CRUD routes for repositories and a route to like a repository

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require('uuidv4')
 
 const app = express();
 
@@ -10,24 +10,60 @@ app.use(cors());
 
 const repositories = [];
 
-app.get("/repositories", (request, response) => {
-  // TODO
+function validateRepositoryid(request, response, next) {
+  const repoIndex = repositories.findIndex(repo => repo.id === request.params.id)
+
+  if (repoIndex < 0) {
+    return response.status(400).json({ error: 'Repository not found.' })
+  }
+
+  return next()
+}
+
+app.use('/repositories/:id', validateRepositoryid)
+
+app.get("/repositories", (_request, response) => {
+  return response.status(200).json(repositories)
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+
+  const repository = { title,  url,  techs, likes: 0, id: uuid() }
+
+  repositories.push(repository)
+
+  return response.status(200).json(repository)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const repoIndex = repositories.findIndex(repo => repo.id === request.params.id)
+  const { title, url, techs } = request.body
+
+  const repository = { ...repositories[repoIndex], title, url, techs }
+
+  repositories[repoIndex] = repository
+
+  return response.status(200).json(repository)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const repoIndex = repositories.findIndex(repo => repo.id === request.params.id)
+
+  repositories.splice(repoIndex, 1)
+
+  return response.status(204).send()
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const repoIndex = repositories.findIndex(repo => repo.id === request.params.id)
+
+  const { likes, ...rest } = repositories[repoIndex]
+  const repository = { ...rest, likes: likes + 1 }
+
+  repositories[repoIndex] = repository
+
+  return response.status(200).json(repository)
 });
 
 module.exports = app;
